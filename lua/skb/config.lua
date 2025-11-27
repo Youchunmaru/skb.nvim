118ba93fe4a7adfe2e7d7ef3e342d99925d298da
Samuel Gröner <samuel.groener@uni-ulm.de>
2025-11-27 13:44:36 +0100

    add path expansion even with overriden configs

--- lua/skb/config.lua
+++ lua/skb/config.lua
@@ -1,9 +1,8 @@
 local M = {}
 
--- 1. The Default Configuration
--- This acts as your schema.
+-- The Default Configuration
 M.defaults = {
-	skb_path = vim.fn.expand("~/knowledge_base"),
+	skb_path = "~/knowledge_base",
 	extension = "md",
 	git = {
 		enabled = true,
@@ -11,17 +10,17 @@ M.defaults = {
 	},
 }
 
--- 2. The Current Configuration
+-- The Current Configuration
 -- Initially, we just set this to the defaults.
 -- When setup() is called, this will be overwritten with merged values.
 M.options = {}
--- 3. The Merge Logic
 function M.setup(opts)
 	opts = opts or {}
 
 	-- vim.tbl_deep_extend is essentially "Object.assign" or a deep merge.
 	-- "force" means the rightmost table (opts) wins if keys conflict.
 	M.options = vim.tbl_deep_extend("force", M.defaults, opts or {})
+	M.options.skb_path = vim.fn.expand(M.options.skb_path) -- expand path to avoid issues later
 end
 
 return M
